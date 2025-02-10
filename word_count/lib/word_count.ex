d6e1f956d9bee7e8c4664a61d6fd58a2a8833934
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-10 10:39:03 +0700

    ignore the book-war-and-peace txt

--- word_count/lib/word_count.ex
+++ word_count/lib/word_count.ex
@@ -1,18 +1,2 @@
 defmodule WordCount do
-  @moduledoc """
-  Documentation for `WordCount`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> WordCount.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
