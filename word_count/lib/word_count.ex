40e6a5f91c13f7a223a941f417b401f048cd0c40
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-10 11:10:31 +0700

    word counts in masterpiece war and peace

--- word_count/lib/word_count.ex
+++ word_count/lib/word_count.ex
@@ -1,2 +1,14 @@
 defmodule WordCount do
+  def start do
+    word_count =
+      File.stream!("book-war-and-peace.txt")
+      |> Stream.flat_map(&split_words/1)
+      |> Enum.count()
+
+    IO.puts("There are total of #{word_count} words in war an peace masterpiece\n")
+  end
+
+  def split_words(line) do
+    line |> String.split(~r/\W+/, trim: true)
+  end
 end
