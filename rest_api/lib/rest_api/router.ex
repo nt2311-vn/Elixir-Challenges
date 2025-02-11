0ae9445aea1a21f582ae70670172ec8864943a0f
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-11 12:05:01 +0700

    define router and get dependencies

---
+++ rest_api/lib/rest_api/router.ex
@@ -0,0 +1,19 @@
+defmodule RestApi.Router do
+  use Plug.Router
+
+  plug(Plug.Router)
+
+  plug(:match)
+
+  plug(Plug.Parsers, parsers: [:json], pass: ["application/json"], json_decoder: Jason)
+
+  plug(:dispatch)
+
+  get "/" do
+    send_resp(conn, 200, "OK")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not found")
+  end
+end
