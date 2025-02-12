eac99cc9ce5f6197c1b640a929e39d2feb836b2a
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-12 09:13:34 +0700

    define a hello function and map to router

--- static_page/lib/static_page_web/router.ex
+++ static_page/lib/static_page_web/router.ex
@@ -18,6 +18,7 @@ defmodule StaticPageWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/hello", PageController, :hello
   end
 
   # Other scopes may use custom stacks.
