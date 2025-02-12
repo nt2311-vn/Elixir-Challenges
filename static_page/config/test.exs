be1f440d6f2b50936445cb4f82ff6f7a934426c5
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-12 10:47:15 +0700

    config db for test case

--- static_page/config/test.exs
+++ static_page/config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :static_page, StaticPage.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "123$%^",
   hostname: "localhost",
   database: "static_page_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
