0319cf79fccad84d8d30d9a86c411f77e9438ea0
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-12 11:32:34 +0700

    made title into an enum base_title

--- static_page/test/static_page_web/controllers/static_page_controller_test.exs
+++ static_page/test/static_page_web/controllers/static_page_controller_test.exs
@@ -1,21 +1,22 @@
 defmodule StaticPageWeb.StaticPageControllerTest do
   use StaticPageWeb.ConnCase
+  @base_title "Phoenix Tutorial Static Page"
 
   test "should get home page", %{conn: conn} do
     conn = get(conn, ~p"/static_pages/home")
 
-    html_response(conn, 200) |> assert_select("title", "Home | Phoenix Tutorial Static Page")
+    html_response(conn, 200) |> assert_select("title", "Home | #{@base_title}")
   end
 
   test "should get help page", %{conn: conn} do
     conn = get(conn, ~p"/static_pages/help")
 
-    html_response(conn, 200) |> assert_select("title", "Help | Phoenix Tutorial Static Page")
+    html_response(conn, 200) |> assert_select("title", "Help | #{@base_title}")
   end
 
   test "should get about page", %{conn: conn} do
     conn = get(conn, ~p"/static_pages/about")
 
-    html_response(conn, 200) |> assert_select("title", "About | Phoenix Tutorial Static Page")
+    html_response(conn, 200) |> assert_select("title", "About | #{@base_title}")
   end
 end
