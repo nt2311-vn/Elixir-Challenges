6e79aa066f1ca40014734071f99da691afd55923
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-13 11:33:20 +0700

    connect websocket to binanance streamer

--- trading_bot/apps/streamer/lib/streamer/binance.ex
+++ trading_bot/apps/streamer/lib/streamer/binance.ex
@@ -1,12 +1,15 @@
-defmodule WebSocketExample do
+defmodule Streamer.Binance do
   use WebSockex
+  @stream_endpoint "wss://stream.binance.com:9443/ws/"
 
-  def start_link(url, state) do
-    WebSockex.start_link(url, __MODULE__, state)
+  def start_link(symbol) do
+    symbol = String.downcase(symbol)
+
+    WebSockex.start_link("#{@stream_endpoint}#{symbol}@trade", __MODULE__, %{})
   end
 
   def handle_frame({type, msg}, state) do
-    IO.puts("Received Message - Type #{inspect(type)} -- Message: #{inspect(msg)}")
+    IO.puts("Received message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
     {:ok, state}
   end
 end
