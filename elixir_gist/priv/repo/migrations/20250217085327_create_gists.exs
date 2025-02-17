ac0c31b0d4e3cf95c0d2b5ec4ef874613639d194
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-17 15:57:56 +0700

    update delete nothing to delete_all

--- elixir_gist/priv/repo/migrations/20250217085327_create_gists.exs
+++ elixir_gist/priv/repo/migrations/20250217085327_create_gists.exs
@@ -3,15 +3,15 @@ defmodule ElixirGist.Repo.Migrations.CreateGists do
 
   def change do
     create table(:gists, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-      add :name, :string
-      add :description, :text
-      add :markup_text, :text
-      add :user_id, references(:users, on_delete: :nothing, type: :binary_id)
+      add(:id, :binary_id, primary_key: true)
+      add(:name, :string)
+      add(:description, :text)
+      add(:markup_text, :text)
+      add(:user_id, references(:users, on_delete: :delete_all, type: :binary_id))
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:gists, [:user_id])
+    create(index(:gists, [:user_id]))
   end
 end
