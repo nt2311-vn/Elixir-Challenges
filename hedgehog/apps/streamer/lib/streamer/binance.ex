8f17ad4e67badb89bb0c981434d8abb0166cc001
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-18 11:13:50 +0700

    start streaming the event

--- hedgehog/apps/streamer/lib/streamer/binance.ex
+++ hedgehog/apps/streamer/lib/streamer/binance.ex
@@ -1,4 +1,5 @@
 defmodule Streamer.Binance do
+  require Logger
   use WebSockex
 
   @stream_endpoint "wss://stream.binance.com:9443/ws/"
@@ -10,8 +11,32 @@ defmodule Streamer.Binance do
   end
 
   def handle_frame({type, msg}, state) do
-    IO.puts("Received message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
+    case Jason.decode(msg) do
+      {:ok, event} -> process_event(event)
+      {:error, _} -> Logger.error("Unable to parse msg: #{msg}")
+    end
+
     {:ok, state}
+  end
+
+  defp process_event(%{"e" => "trade"} = event) do
+    trade_event = %Streamer.Binance.TradeEvent{
+      :event_type => event["e"],
+      :event_time => event["E"],
+      :symbol => event["s"],
+      :trade_id => event["t"],
+      :price => event["p"],
+      :quantity => event["q"],
+      :buyer_order_id => event["b"],
+      :seller_order_id => event["a"],
+      :trade_time => event["T"],
+      :buyer_market_maker => event["m"]
+    }
+
+    Logger.debug(
+      "Trade event received " <>
+        "#{trade_event.symbol}@#{trade_event.price}@#{trade_event.quantity}"
+    )
   end
 
   def handle_cast({:send, {type, msg} = frame}, state) do
