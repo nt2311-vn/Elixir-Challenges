fead4121d239c12f747c5a1e190a8279e6d67675
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-20 15:05:44 +0700

    using spawn_monitor to get udpate when an process is crashed without linking to it or using Process.flags

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -15,11 +15,17 @@ defmodule Example do
   def explode(), do: exit("kaboom !!!")
 
   def run do
-    Process.flag(:trap_exit, true)
-    spawn_link(Example, :explode, [])
+    # Process.flag(:trap_exit, true)
+    # spawn_link(Example, :explode, [])
+    #
+    # receive do
+    #   {:EXIT, _from_pid, reason} -> IO.puts("Exit reason: #{reason}")
+    # end
+
+    spawn_monitor(Example, :explode, [])
 
     receive do
-      {:EXIT, _from_pid, reason} -> IO.puts("Exit reason: #{reason}")
+      {:DOWN, _ref, :process, _from_pid, reason} -> IO.puts("Exit reason: #{reason}")
     end
   end
 end
