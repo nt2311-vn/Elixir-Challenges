d482be95ccba89f813a644bbf24f5a4342f67802
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-20 15:38:11 +0700

    implement a simple queue to understand elixir GenServer

--- simplequeue/lib/simplequeue.ex
+++ simplequeue/lib/simplequeue.ex
@@ -15,4 +15,19 @@ defmodule Simplequeue do
   """
 
   def init(state), do: {:ok, state}
+
+  @doc """
+  GenServer.handle_call/3 callback
+  """
+
+  def handle_call(:dequeue, _from, [value | state]) do
+    {:reply, value, state}
+  end
+
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
+
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
 end
