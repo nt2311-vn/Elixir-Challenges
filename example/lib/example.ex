4b7ead3cb5238cf72b96b8f9f8d3e5ca7aa9399c
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-20 15:01:58 +0700

    using Process.flag to not crash other link process

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -13,4 +13,13 @@ defmodule Example do
   end
 
   def explode(), do: exit("kaboom !!!")
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(Example, :explode, [])
+
+    receive do
+      {:EXIT, _from_pid, reason} -> IO.puts("Exit reason: #{reason}")
+    end
+  end
 end
