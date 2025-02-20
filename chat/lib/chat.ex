369807f14c72f72c6d5a869494dfe91e88bbe0a2
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-20 17:21:59 +0700

    pattern matching on case of moebi to spawn different receive function

--- chat/lib/chat.ex
+++ chat/lib/chat.ex
@@ -3,17 +3,19 @@ defmodule Chat do
     IO.puts(msg)
   end
 
-  def receive_message_for_moebi(msg, from) do
-    IO.puts(msg)
-    send_message(from, "chicken?")
-  end
-
   def send_message(recipient, msg) do
-    spawn_task(__MODULE__, :receive_message, recipient, [msg])
+    case recipient do
+      :moebi@localhost ->
+        spawn_task(__MODULE__, :receive_message_for_moebi, :moebi@localhost, [msg, Node.self()])
+
+      _ ->
+        spawn_task(__MODULE__, :receive_message, recipient, [msg])
+    end
   end
 
-  def send_message(:moebi@localhost, msg) do
-    spawn_task(__MODULE__, :receive_message_for_moebi, :moebi@localhost, [msg, Node.self()])
+  def receive_message_for_moebi(message, from) do
+    IO.puts(message)
+    send_message(from, "chicken?")
   end
 
   def spawn_task(module, fun, recipient, args) do
