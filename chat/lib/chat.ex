d0f2d184b78dad37ae082db2bfc228f379eb0ac8
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-20 16:56:40 +0700

    create send and receive message to test how distribution work

--- chat/lib/chat.ex
+++ chat/lib/chat.ex
@@ -1,2 +1,17 @@
 defmodule Chat do
+  def receive_message(msg) do
+    IO.puts(msg)
+  end
+
+  def send_message(recipient, msg) do
+    spawn_task(__MODULE__, :receive_msg, recipient, [msg])
+  end
+
+  def spawn_task(module, fun, recipient, args) do
+    recipient |> remote_supervisor() |> Task.Supervisor.async(module, fun, args) |> Task.await()
+  end
+
+  defp remote_supervisor(recipient) do
+    {Chat.TaskSupervisor, recipient}
+  end
 end
