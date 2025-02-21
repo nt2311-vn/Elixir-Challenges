167a35bab16e61fdb7f673da23313d72474ba73a
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-21 10:47:01 +0700

    when a module with behaviour from a defined module with callback it must implement those features

--- example/lib/downloader.ex
+++ example/lib/downloader.ex
@@ -1,2 +1,18 @@
 defmodule Example.Downloader do
+  @behaviour Example.Worker
+
+  def init(opts), do: {:ok, opts}
+
+  def perform(url, opts) do
+    url
+    |> HTTPoison.get!()
+    |> Map.fetch(:body)
+    |> write_file(opts[:path])
+    |> respond(opts)
+  end
+
+  defp write_file(:error, _), do: {:eror, :missing_body}
+
+  defp respond(:ok, opts), do: {:ok, opts[:path], opts}
+  defp respond({:error, reason}, opts), do: {:error, reason, opts}
 end
