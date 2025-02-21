a661b6da8d4790a8509b386d268a857c4bb17d27
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-21 15:47:03 +0700

    make name field required when making change on person

--- friends/lib/friends/person.ex
+++ friends/lib/friends/person.ex
@@ -1,8 +1,13 @@
 defmodule Friends.Person do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "people" do
     field(:name, :string)
     field(:age, :integer, default: 0)
+  end
+
+  def changeset(struct, params) do
+    struct |> cast(params, [:name, :age]) |> validate_required([:name])
   end
 end
