000327c7dd1b01bc8e8db04eb99ba5d49a18240a
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-21 10:18:54 +0700

    create worker behavior and downloader to follow the worker behavior

---
+++ example/lib/worker.ex
@@ -0,0 +1,6 @@
+defmodule Example.Worker do
+  @callback init(state :: term) :: {:ok, new_state :: term} | {:error, reason :: term}
+  @callback perform(args :: term, state :: term) ::
+              {:ok, result :: term, new_state :: term}
+              | {:error, reason :: term, new_state :: term}
+end
