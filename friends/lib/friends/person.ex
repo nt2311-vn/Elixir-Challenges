f51d8090cf1e5728332fd34d23b07b4c8e36afee
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-21 16:00:24 +0700

    create a changeset rule automatically to set name to Anonymous if there is no name provided when registers

--- friends/lib/friends/person.ex
+++ friends/lib/friends/person.ex
@@ -18,11 +18,27 @@ defmodule Friends.Person do
     end
   end
 
+  def set_name_if_anonymous(changeset) do
+    name = get_field(changeset, :name)
+
+    if is_nil(name) do
+      put_change(changeset, :name, "Anonymous")
+    else
+      changeset
+    end
+  end
+
   def changeset(struct, params) do
     struct
     |> cast(params, [:name, :age])
     |> validate_required([:name])
     |> validate_length(:name, min: 2)
     |> validate_fictional_name()
+  end
+
+  def registration_changeset(struct, params) do
+    struct
+    |> cast(params, [:name, :age])
+    |> set_name_if_anonymous()
   end
 end
