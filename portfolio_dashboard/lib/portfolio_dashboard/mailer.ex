f6c66ff76dfedbb6a7b670a8d6be1aae1162215d
nt2311 <nguyentoan231196@gmail.com>
2025-02-23 14:38:39 +0700

    set up a new portfolio project

---
+++ portfolio_dashboard/lib/portfolio_dashboard/mailer.ex
@@ -0,0 +1,3 @@
+defmodule PortfolioDashboard.Mailer do
+  use Swoosh.Mailer, otp_app: :portfolio_dashboard
+end
---
+++ portfolio_dashboard/test/portfolio_dashboard_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule PortfolioDashboardWeb.PageControllerTest do
+  use PortfolioDashboardWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
