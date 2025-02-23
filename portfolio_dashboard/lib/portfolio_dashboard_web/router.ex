3780fecfa430f13e00d7c0adf26a8a0b28094a3f
nt2311 <nguyentoan231196@gmail.com>
2025-02-23 15:19:23 +0700

    add the auth to router.exc

--- portfolio_dashboard/lib/portfolio_dashboard_web/router.ex
+++ portfolio_dashboard/lib/portfolio_dashboard_web/router.ex
@@ -2,28 +2,36 @@ defmodule PortfolioDashboardWeb.Router do
   use PortfolioDashboardWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {PortfolioDashboardWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {PortfolioDashboardWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", PortfolioDashboardWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", PortfolioDashboardWeb do
   #   pipe_through :api
   # end
+
+  # Authentication Scope for Google OAuth
+  scope "/auth", PortfolioDashboardWeb do
+    pipe_through(:browser)
+
+    get("/:provider", AuthController, :request)
+    get("/:provider/callback", AuthController, :callback)
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:portfolio_dashboard, :dev_routes) do
@@ -35,10 +43,10 @@ defmodule PortfolioDashboardWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: PortfolioDashboardWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: PortfolioDashboardWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
