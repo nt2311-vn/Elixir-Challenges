eb353fd807d993f04eb2f50955ef4381a9ad0ddc
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-24 11:18:38 +0700

    delete my portfolio project as moving to private

--- my_portfolio/lib/my_portfolio/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule MyPortfolio.Mailer do
-  use Swoosh.Mailer, otp_app: :my_portfolio
-end
--- my_portfolio/test/my_portfolio_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MyPortfolioWeb.PageControllerTest do
-  use MyPortfolioWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
